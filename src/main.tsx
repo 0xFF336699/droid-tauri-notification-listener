16b5973bf1ebc766dfcf4e063ca246a4c8054ceb
0xFF336699 <0xFF336699@gmail.com>
2025-10-13 20:42:58 +0800

    换了icon

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import "./i18n"; // 导入i18n配置
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
